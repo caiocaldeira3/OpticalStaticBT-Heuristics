977211c745c5a7a817911c08c5c0d7673ba61738
caiocaldeira3 <caio.caldeira3@gmail.com>
2025-02-10 17:15:25 +0100

    Implement nem main infrastructure to test new algorithms

---
+++ include/manager.hh
@@ -0,0 +1,50 @@
+#pragma once
+
+
+#include "util.hh"
+#include "optbst.hh"
+#include "graphbissection.hh"
+
+
+struct Response_t {
+    double cost;
+    std::vector<int> graphOrdering;
+};
+
+Response_t testRawGraphBissection (int nVertices, const std::vector<std::vector<double>>& demandMatrix) {
+    std::vector<int> vertices(nVertices);
+    for (int vIdx = 0; vIdx < nVertices; vIdx++) {
+        vertices[vIdx] = vIdx;
+    }
+
+    VectorLimits_t vectorLimits = { 0, nVertices };
+    int maxDepth = ceil(log(nVertices) / log(2)) + 1;
+
+    graphReordering(demandMatrix, vertices, vectorLimits, INF);
+
+    std::vector<std::vector<int>> tree(nVertices, std::vector<int>());
+    buildBalancedBinaryTree(vertices, tree, vectorLimits, -1);
+
+    return {
+        NEWtreeCost(tree, demandMatrix),
+        vertices
+    };
+}
+
+Response_t testBissectionPlusOBST (int nVertices, const std::vector<std::vector<double>>& demandMatrix) {
+    std::vector<int> vertices(nVertices);
+    for (int vIdx = 0; vIdx < nVertices; vIdx++) {
+        vertices[vIdx] = vIdx;
+    }
+
+    VectorLimits_t vectorLimits = { 0, nVertices };
+    int maxDepth = ceil(log(nVertices) / log(2)) + 1;
+
+    graphReordering(demandMatrix, vertices, vectorLimits, INF);
+    std::vector<std::vector<double>> reorderedDemand = reconfigureDemandMatrix(vertices, demandMatrix);
+
+    return {
+        optimalBST(nVertices, reorderedDemand, true),
+        vertices
+    };
+}
---
+++ src/newMain.cc
@@ -0,0 +1,115 @@
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <random>
+#include <chrono>
+
+#include "argparse/argparse.hh"
+#include "manager.hh"
+
+int main (int argc, char* argv[]) {
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    srand(seed);
+
+    argparse::ArgumentParser parser("main_args");
+
+    std::string inputName;
+    parser.add_argument("input-name")
+        .store_into(inputName)
+        .help("the name of the input in weights folder");
+
+    int testNumber;
+    parser.add_argument("--test-number")
+        .default_value(0)
+        .store_into(testNumber)
+        .help("the test number");
+
+    try {
+        parser.parse_args(argc, argv);
+
+    } catch (const std::exception& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << parser;
+        std::exit(1);
+    }
+
+    std::ifstream iFile("weights/" + inputName);
+    int nVertices;
+    iFile >> nVertices;
+
+    int src, dst;
+    double flowSize;
+    std::vector<std::vector<double>> demandMatrix(nVertices, std::vector<double>(nVertices, 0.0));
+    while (iFile >> src >> dst >> flowSize) {
+        demandMatrix[src][dst] = flowSize;
+    }
+
+    std::string baseFolderName = ("output/" + inputName + "/");
+
+    namespace fs = std::filesystem;
+    fs::create_directories(baseFolderName + "raw/");
+    fs::create_directories(baseFolderName + "obst-bissection/");
+
+    {
+        std::ofstream rawCostFile(baseFolderName + "raw/" + std::to_string(testNumber) );
+
+        const clock_t rawBeginTime = std::clock();
+        Response_t rawBissecResponse = testRawGraphBissection(nVertices, demandMatrix);
+        double rawSec = double(std::clock() - rawBeginTime) / CLOCKS_PER_SEC;
+
+        std::cout << "Raw Bissection Cost: " << rawBissecResponse.cost << std::endl;
+        std::cout << "Raw Time Spent: " << rawSec << std::endl;
+
+        std::ofstream rawCostsFile(
+            baseFolderName + "raw_costs.out", std::ios_base::app
+        );
+        rawCostsFile << rawBissecResponse.cost << std::endl;
+
+        std::ofstream rawTimeSpent(
+            baseFolderName + "raw_time_spent.out", std::ios_base::app
+        );
+        rawTimeSpent << rawSec << std::endl;
+
+        std::ofstream rawOrderingFile(
+            baseFolderName + "raw/ordering_" + std::to_string(testNumber) + ".out", std::ios_base::app
+        );
+        for (int vIdx = 0; vIdx < nVertices; vIdx++) {
+            rawOrderingFile << rawBissecResponse.graphOrdering[vIdx] << " ";
+        }
+        rawOrderingFile << std::endl;
+    }
+
+    {
+        std::ofstream obstCostFile(baseFolderName + "obst-bissection/" + std::to_string(testNumber) );
+
+        const clock_t obstBeginTime = std::clock();
+        Response_t obstBissecResponse = testBissectionPlusOBST(nVertices, demandMatrix);
+        double obstSec = double(std::clock() - obstBeginTime) / CLOCKS_PER_SEC;
+
+        std::cout << "OBST Bissection Cost: " << obstBissecResponse.cost << std::endl;
+        std::cout << "OBST Time Spent: " << obstSec << std::endl;
+
+        std::ofstream obstCostsFile(
+            baseFolderName + "obst-bissection_costs.out", std::ios_base::app
+        );
+        obstCostsFile << obstBissecResponse.cost << std::endl;
+
+        std::ofstream obstTimeSpent(
+            baseFolderName + "obst-bissection_time_spent.out", std::ios_base::app
+        );
+        obstTimeSpent << obstSec << std::endl;
+
+        std::ofstream obstOrderingFile(
+            baseFolderName + "obst-bissection/ordering_" + std::to_string(testNumber) + ".out", std::ios_base::app
+        );
+        for (int vIdx = 0; vIdx < nVertices; vIdx++) {
+            obstOrderingFile << obstBissecResponse.graphOrdering[vIdx] << " ";
+        }
+        obstOrderingFile << std::endl;
+    }
+}
+
+
+
