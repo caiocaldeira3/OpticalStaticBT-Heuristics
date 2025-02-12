ea8942bac6096983f0b011bb656eea528af7c7eb
caiocaldeira3 <caio.caldeira3@gmail.com>
2025-02-12 16:25:56 +0100

    Add entropy computation for the demand matrix

---
+++ include/bounds.hh
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <vector>
+#include <util.hh>
+
+
+struct ConditionalEntropy_T {
+    double entropyXgivenY;
+    double entropyYgivenX;
+};
+
+struct MarginalEntropy_T {
+    double entropyX;
+    double entropyY;
+};
+
+double computeJointEntropy (
+    const std::vector<std::vector<double>>& demandMatrix
+) {
+    double jointEntropy = 0;
+    for (auto& row: demandMatrix) {
+        for (double demand: row) {
+            if (demand > 0)
+                jointEntropy += demand * log2(demand);
+        }
+    }
+
+    return -jointEntropy;
+}
+
+ConditionalEntropy_T computeConditionalEntropy (
+    const std::vector<std::vector<double>>& demandMatrix
+) {
+    int nVertices = demandMatrix.size();
+    std::vector<double> rowSums(nVertices, 0);
+    std::vector<double> colSums(nVertices, 0);
+    ConditionalEntropy_T conditionalEntropy{0, 0};
+    double demand;
+
+    for (int src = 0; src < nVertices; src++) {
+        for (int dst = 0; dst < demandMatrix[src].size(); dst++) {
+            rowSums[src] += demandMatrix[src][dst];
+            colSums[dst] += demandMatrix[src][dst];
+
+        }
+    }
+
+    for (int src = 0; src < nVertices; src++) {
+        for (int dst = 0; dst < demandMatrix[src].size(); dst++) {
+            demand = demandMatrix[src][dst];
+            if (demand > 0) {
+                conditionalEntropy.entropyYgivenX -= demand * log2(demand / rowSums[src]);
+                conditionalEntropy.entropyXgivenY -= demand * log2(demand / colSums[dst]);
+            }
+        }
+    }
+
+    return conditionalEntropy;
+}
+
+MarginalEntropy_T computeMarginalEntropy (
+    const std::vector<std::vector<double>>& demandMatrix
+) {
+    std::vector<double> rowSums(demandMatrix.size(), 0);
+    std::vector<double> colSums(demandMatrix[0].size(), 0);
+    MarginalEntropy_T marginalEntropy{0, 0};
+
+    for (int i = 0; i < demandMatrix.size(); i++) {
+        for (int j = 0; j < demandMatrix[i].size(); j++) {
+            rowSums[i] += demandMatrix[i][j];
+            colSums[j] += demandMatrix[i][j];
+        }
+    }
+
+    for (int i = 0; i < rowSums.size(); i++) {
+        if (rowSums[i] > 0) {
+            marginalEntropy.entropyX -= rowSums[i] * log2(rowSums[i]);
+        }
+
+        if (colSums[i] > 0) {
+            marginalEntropy.entropyY -= colSums[i] * log2(colSums[i]);
+        }
+    }
+
+    return marginalEntropy;
+}
---
+++ src/getMatrixEntropy.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
+
+#include <argparse/argparse.hh>
+#include <bounds.hh>
+
+int main (int argc, char* argv[]) {
+    argparse::ArgumentParser parser("entropy_args");
+
+    std::string inputName;
+    parser.add_argument("input-name")
+        .store_into(inputName)
+        .help("the name of the input in weights folder");
+
+    try {
+        parser.parse_args(argc, argv);
+
+    } catch (const std::exception& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << parser;
+        std::exit(1);
+    }
+
+    std::ifstream iFile("weights/" + inputName);
+    int nVertices;
+    iFile >> nVertices;
+
+    int src, dst;
+    double flowSize;
+    std::vector<std::vector<double>> demandMatrix(nVertices, std::vector<double>(nVertices, 0.0));
+    while (iFile >> src >> dst >> flowSize) {
+        demandMatrix[src][dst] = flowSize;
+    }
+
+    std::cout << "Joint Entropy (X, Y): " << computeJointEntropy(demandMatrix) << std::endl;
+
+    std::cout << "Conditional Entropy (X|Y) and (Y|X):" << std::endl;
+    ConditionalEntropy_T conditionalEntropy = computeConditionalEntropy(demandMatrix);
+    std::cout << "\tE(X|Y): " << conditionalEntropy.entropyXgivenY << std::endl;
+    std::cout << "\tE(Y|X): " << conditionalEntropy.entropyYgivenX << std::endl;
+    std::cout << "\tE(X|Y) + E(Y|X): " << conditionalEntropy.entropyXgivenY + conditionalEntropy.entropyYgivenX << std::endl;
+
+    std::cout << "Marginal Entropy (X) and (Y):" << std::endl;
+    MarginalEntropy_T marginalEntropy = computeMarginalEntropy(demandMatrix);
+    std::cout << "\tE(X): " << marginalEntropy.entropyX << std::endl;
+    std::cout << "\tE(Y): " << marginalEntropy.entropyY << std::endl;
+    std::cout << "\tE(X) + E(Y): " << marginalEntropy.entropyX + marginalEntropy.entropyY << std::endl;
+
+    double mutualInformation = (
+        marginalEntropy.entropyX + marginalEntropy.entropyY -
+        conditionalEntropy.entropyXgivenY - conditionalEntropy.entropyYgivenX
+    );
+    std::cout << "Mutual Information (X; Y): " << mutualInformation << std::endl;
+
+}
